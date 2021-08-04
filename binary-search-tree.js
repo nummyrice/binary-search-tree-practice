591ad4a7bbf18f3d210fe70d4c899fe406ef292c
iethan-h <ethanharwell34@gmail.com>
2021-08-04 10:56:46 -0500

    solved insert

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -1,67 +1,65 @@
 // Do not change this
 class TreeNode {
-  constructor(val) {
-    this.val = val;
-    this.left = null;
-    this.right = null;
-  }
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
 }
 
 class BinarySearchTree {
 
-  constructor() {
-    // Fill this in
-    this.root = null;
-    // sets default to null
-  }
-
-  insert(val) {
-    // Fill this in
-    const node = this.root;
-    if (node === null) {
-      this.root = new TreeNode(val);
-      // if root has no value, this puts a value in the root
-      return;
-    } else {
-      const treeSearch = function(node) {
-        if (val < node.val) {
-          // searches to left side of the tree
-          if (node.left === null) {
-            node.left = new TreeNode(val);
-            return;
-          } else if (node.left !== null) {
-            return treeSearch(node.left);
-          }
-        } else if (val >= node.val) {
-          // searches to the right of the tree
-          if (node.right === null) {
-            node.right = new TreeNode(val);
-            return;
-          } else if (node.right !== null) {
-            return treeSearch(node.right);
-          }
-        } else {
-          return null;
-        }
-      }
+    constructor() {
+        // Fill this in
+        this.root = null;
+        // sets default to null
     }
-  }
 
-  search(val) {
-    // Fill this in
-  }
+    insert(val) {
 
-  preOrderTraversal() {
-    // Fill this in
-  }
+        const node = this.root
+        if (node === null) {
+            this.root = new TreeNode(val); //IF the root node is null, we set the new value to the null node.
+            return;
+        } else {
+            const search = function(node) {
+                if (val < node.val) { //left side of the tree
+                    if (node.left === null) {
+                        node.left = new TreeNode(val); //Puts values to the left side of the tree IF the node is null.
+                        return;
+                    } else if (node.left !== null) {
+                        return search(node.left); //This continues searching to the left until the node is null.
+                    }
+                } else if (val > node.val) { //right side of the tree
+                    if (node.right === null) {
+                        node.right = new TreeNode(val); //Puts values to the right side of the tree IF the node is null.
+                        return;
+                    } else if (node.right !== null) {
+                        return search(node.right) //This continues searching to the right until the node is null.
+                    }
+                } else {
+                    return null;
+                }
+            }
+            return search(node)
+        }
+    }
 
-  inOrderTraversal() {
-    // Fill this in
-  }
+    search(val) {
+        // Fill this in
+    }
 
-  postOrderTraversal() {
-    // Fill this in
-  }
+    preOrderTraversal() {
+        // Fill this in
+    }
+
+    inOrderTraversal() {
+        // Fill this in
+    }
+
+    postOrderTraversal() {
+        // Fill this in
+    }
 
 }
 
