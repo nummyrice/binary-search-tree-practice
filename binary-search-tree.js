2593ea178c4e064f906533470b8973b9e3ea3c8e
nummyrice <nicholasroyrice@gmail.com>
2021-08-04 08:42:36 -0700

    first two test complete

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -11,10 +11,39 @@ class BinarySearchTree {
 
   constructor() {
     // Fill this in
+    this.root = null;
+    // sets default to null
   }
 
   insert(val) {
     // Fill this in
+    const node = this.root;
+    if (node === null) {
+      this.root = new TreeNode(val);
+      // if root has no value, this puts a value in the root
+      return;
+    } else {
+      const treeSearch = function(node) {
+        if (val < node.val) {
+          // searches to left side of the tree
+          if (node.left === null) {
+            node.left = new TreeNode(val);
+            return;
+          } else if (node.left !== null) {
+            return treeSearch(node.left);
+          }
+        } else if (val > node.val) {
+          if (node.right === null) {
+            node.right = new TreeNode(val);
+            return;
+          } else if (node.right !== null) {
+            return treeSearch(node.right);
+          }
+        } else {
+          return null;
+        }
+      }
+    }
   }
 
   search(val) {
