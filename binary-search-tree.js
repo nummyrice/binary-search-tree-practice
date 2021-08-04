0347b3e5520cc7cee0c1d29780ca6cabaef3ffe1
iethan-h <ethanharwell34@gmail.com>
2021-08-04 10:59:33 -0500

    changed name of search function

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -22,31 +22,32 @@ class BinarySearchTree {
             this.root = new TreeNode(val); //IF the root node is null, we set the new value to the null node.
             return;
         } else {
-            const search = function(node) {
+            const searchTree = function(node) {
                 if (val < node.val) { //left side of the tree
                     if (node.left === null) {
                         node.left = new TreeNode(val); //Puts values to the left side of the tree IF the node is null.
                         return;
                     } else if (node.left !== null) {
-                        return search(node.left); //This continues searching to the left until the node is null.
+                        return searchTree(node.left); //This continues searchTreeing to the left until the node is null.
                     }
                 } else if (val > node.val) { //right side of the tree
                     if (node.right === null) {
                         node.right = new TreeNode(val); //Puts values to the right side of the tree IF the node is null.
                         return;
                     } else if (node.right !== null) {
-                        return search(node.right) //This continues searching to the right until the node is null.
+                        return searchTree(node.right) //This continues searching to the right until the node is null.
                     }
                 } else {
                     return null;
                 }
             }
-            return search(node)
+            return searchTree(node)
         }
     }
 
     search(val) {
         // Fill this in
+
     }
 
     preOrderTraversal() {
